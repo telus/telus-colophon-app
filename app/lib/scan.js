103cafb581b7fc575ded0eba313ade353bc97978
Enrico Sacchetti <enrico.sacchetti@telus.com>
2019-02-08 16:21:19 -0500

    feat(app): show last updated date for colophon file

--- app/lib/scan.js
+++ app/lib/scan.js
@@ -10,8 +10,12 @@ let names = [
 
 // TODO: use regex to search for colophon file against all repo files
 
-module.exports = (github, { owner, repo }) => {
-  // attempt to load all possible variations
+// getColophonPath = async(github, owner, repo) => {
+//   return github
+//     .listFiles({owner, repo, })
+// }
+
+getColophonContents = async (github, owner, repo) => {
   return Promise.all(names.map(name => {
     return github
       .repos
@@ -28,4 +32,23 @@ module.exports = (github, { owner, repo }) => {
 
     // filter out empty results
     .then(entries => entries.filter(entry => entry.content))
+}
+
+getLastUpdatedDate = async (github, owner, repo, path) => {
+  return github
+    .repos
+    .listCommits({owner, repo, path, per_page: 1})
+
+    .then(({data}) => {
+      return data[0].commit.committer.date
+    })
+}
+
+module.exports = async (github, { owner, repo }) => {
+  // attempt to load all possible variations
+    let colophonContents = await getColophonContents(github, owner, repo)
+
+    colophonContents[0].lastUpdated = await getLastUpdatedDate(github, owner, repo, colophonContents[0].name)
+
+    return colophonContents
 }
