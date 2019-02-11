d623ba6c3edd222d740f5300aeaa470d39b8a714
Ahmad Nassri <ahmad@ahmadnassri.com>
2019-02-11 15:32:40 -0500

    feat(db): improved db schema

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,13 @@
+-- trigger to update timestamp
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- repositories table
 CREATE TABLE repositories (
   id VARCHAR PRIMARY KEY,
   installation VARCHAR,
@@ -5,6 +15,13 @@ CREATE TABLE repositories (
   name VARCHAR,
   private BOOL NOT NULL DEFAULT TRUE,
   filename VARCHAR,
+  updated TIMESTAMP NOT NULL DEFAULT NOW(),
   content TEXT,
   colophon jsonb
 );
+
+-- automatically update timestamp
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON repositories
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
